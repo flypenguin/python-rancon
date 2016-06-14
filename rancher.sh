893061ab8f05e55f8b1cf6e326ed319c637a15ea
Axel Bock <mr.axel.bock@gmail.com>
2016-06-14 23:13:14 +0200

    auto-detect environment setting using rancher metadata service

--- rancher.sh
+++ rancher.sh
@@ -7,6 +7,12 @@ ADD_PARAMS=""
 if [ ! -z "$CATTLE_URL" ]; then
     echo "STARTUP: detected CATTLE_URL: $CATTLE_URL"
     ADD_PARAMS="-s url=$CATTLE_URL"
+    # we just assume we have rancher metadata access
+    ENV_NAME=$(curl http://rancher-metadata/latest/self/stack/environment_name 2>/dev/null)
+    if [ "$?" == "0" ]; then
+        echo "STARTUP: Found environment '$ENV_NAME'"
+        ADD_PARAMS=" $ADD_PARAMS -i $ENV_NAME"
+    fi
 fi
 
 if [ ! -z "$CATTLE_ACCESS_KEY" -a ! -z "$CATTLE_SECRET_KEY" ]; then
