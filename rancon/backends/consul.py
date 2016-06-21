47d09df0684645d7fdcdfceb5cb12a9f6f091492
Axel Bock <mr.axel.bock@gmail.com>
2016-06-21 13:51:40 +0200

    lowercase everything with consul

--- rancon/backends/consul.py
+++ rancon/backends/consul.py
@@ -1,9 +1,9 @@
-from rancon import settings
 from rancon.tools import tag_replace, getLogger
 from . import BackendBase
 
 import consul
 
+from re import sub as resub
 from urllib.parse import urlparse as up
 
 
@@ -16,38 +16,40 @@ class ConsulBackend(BackendBase):
                  id_schema='%NAME%_%HOST%_%PORT%',
                  cleanup_id='default'):
         self.log = getLogger(__name__)
-        self.log.error("INIT: url={}".format(url))
-        self.log.error("INIT: id_schema={}".format(id_schema))
-        self.log.error("INIT: cleanup_id={}".format(cleanup_id))
         parsed_url = up(url)
         items = parsed_url.netloc.split(":")
         # can be a list.
         self.id_schema = id_schema
-        self.cleanup_id = cleanup_id
+        self.cleanup_id = cleanup_id.lower()
         if len(items) == 1:
             self.consul = consul.Consul(host=parsed_url.netloc,
                                         scheme=parsed_url.scheme)
         else:
             self.consul = consul.Consul(host=items[0], port=items[1],
                                         scheme=parsed_url.scheme)
+        self.log.error("INIT: url={}".format(url))
+        self.log.error("INIT: id_schema={}".format(self.id_schema))
+        self.log.error("INIT: cleanup_id={}".format(self.cleanup_id))
 
     def register(self, service):
-        svc_id = tag_replace(self.id_schema, service)
+        # lower everything, consul should not have upper/lower case distinction
+        svc_id = self._get_service_id(service)
+        svc_name = service.name.lower()
         success = self.consul.agent.service.register(
-            service.name,
+            svc_name,
             svc_id,
             address=service.host, port=int(service.port),
             tags=self._get_tags(service),
         )
         if success:
             self.log.warn("REGISTER: {} using {} / {} (cleanup id: {})"
-                          .format(service, service.name, svc_id,
+                          .format(service, svc_name, svc_id,
                                   self._get_cleanup_tag()))
             return svc_id
         else:
             self.log.warn("REGISTER: FAILED registering "
                           "service {} using {} / {}"
-                          .format(service, service.name, svc_id))
+                          .format(service, svc_name, svc_id))
             return None
 
     def cleanup(self, keep_services):
@@ -64,13 +66,16 @@ class ConsulBackend(BackendBase):
     def _get_tags(self, service):
         tag_list_str = service.get('tag', '')
         tag_list = tag_list_str.split(",") if tag_list_str else []
-        tmp =  [tag_replace(x, service).strip() for x in tag_list] + \
+        return [tag_replace(x, service).strip().lower() for x in tag_list] + \
                [self._get_cleanup_tag(),
                 'rancon']
-        return tmp
 
     def _get_cleanup_tag(self):
-        return "cleanup_id_{}".format(self.cleanup_id)
+        return "rancon_cleanup_id_{}".format(self.cleanup_id)
+
+    def _get_service_id(self, service):
+        tmp = tag_replace(self.id_schema, service).lower()
+        return resub(r"[^a-z0-9-]", "-", tmp)
 
 
 def get():
