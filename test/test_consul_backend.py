621457682c27955d07ad5c9798a767aff482c27d
Axel Bock <mr.axel.bock@gmail.com>
2017-06-07 12:24:48 +0200

    fix unit test

--- test/test_consul_backend.py
+++ test/test_consul_backend.py
@@ -1,15 +1,15 @@
 from dotmap import DotMap
 
 from unittest import TestCase
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, call
 
 from rancon.backends import consul
 
 TEST_SERVICES = [DotMap({
     'name': sname,
     'host': 'host_{}'.format(sname),
-    'port': str(10001 + i),
-}) for i, sname in enumerate(('one', 'two', 'three'))]
+    'port': str(101 + i),
+}) for i, sname in enumerate(('1', '2', '3'))]
 
 
 class TestConsulStatic(TestCase):
@@ -43,21 +43,18 @@ class TestConsulDynamic(TestCase):
         _, con1 = self.consul.register(TEST_SERVICES[1])
         self.assertEqual(2, len(self.consul.consul_inst_cache))
         # check return values
-        self.assertEqual('one-host-one-10001', con0)
-        self.assertEqual('two-host-two-10002', con1)
+        self.assertEqual('1-host-1-101', con0)
+        self.assertEqual('2-host-2-102', con1)
         # check calls
         self.assertTrue(consul_mock.called)
         # nice:
         # http://stackoverflow.com/questions/7242433/asserting-successive-calls-to-a-mock-method
-        consul_mock.assert_any_call(host="host_one", port=8500, scheme='http')
-        consul_mock.assert_any_call(host="host_two", port=8500, scheme='http')
+        consul_mock.assert_any_call(host="host_1", port=8500, scheme='http')
+        consul_mock.assert_any_call(host="host_2", port=8500, scheme='http')
         self.assertEqual(2, consul_mock.call_count)
         self.assertTrue(mock_agent.called)
-        mock_agent.assert_any_call('one', 'one-host-one-10001',
-                             address='host_one',
-                             port=10001,
-                             tags=['rancon-cleanup-id-default', 'rancon'])
-        mock_agent.assert_any_call('two', 'two-host-two-10002',
-                             address='host_two',
-                             port=10002,
-                             tags=['rancon-cleanup-id-default', 'rancon'])
+        taglist = ['rancon-default']
+        mock_agent.assert_has_calls((
+            call('1', '1-host-1-101', address='host_1', port=101, tags=taglist),
+            call('2', '2-host-2-102', address='host_2', port=102, tags=taglist)
+        ))
